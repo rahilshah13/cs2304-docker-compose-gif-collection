4649939c5f9365efdcceb584d5a7bf7c4453026a
rahilshah13 <rahil@vt.edu>
2021-03-19 18:10:43 -0400

    added prometheus

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const MONGO_URL = "mongodb://mongo:27017";
+const prometheusClient = require("prom-client");
+const prometheusMiddleware = require("express-prometheus-middleware");
+
+////////////////// prometheus metrics
+const gifsAddedCounter = new prometheusClient.Counter({
+    name: "app_gifs_added_total",
+    help: "The total number of GIFs that have been added to the database",
+});
+
+const mongoQuerySummary = new prometheusClient.Summary({
+    name: "app_mongo_query_time",
+    help: "Query times to get the next Mongo image",
+});
+/////////////////////////////////
 
 MongoClient.connect(MONGO_URL, (err, db) => {
   if (err) throw err;
@@ -20,10 +34,16 @@ MongoClient.connect(MONGO_URL, (err, db) => {
     extended: true
   }));
 
+  app.use(prometheusMiddleware());
+
+
   // Add a handler for requests to "/". Simply picks a random image and renders the template.
   app.get("/", async (req, res) => {
+    const endTimer = mongoQuerySummary.startTimer();
     const cursor = gifCollection.aggregate([{ $sample: { size : 1 }}]);
-    const data = await cursor.next();
+    const data = await cursor.next(); 
+    endTimer();
+
     const imageUrl = (data) ? data.url : "https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif";
     res.render("index", { imageUrl, message: MY_MESSAGE });
   });
@@ -31,6 +51,7 @@ MongoClient.connect(MONGO_URL, (err, db) => {
   app.post("/add-gif", async (req, res) => {
     const newDocument = { url : req.body.url, default: false };
     await gifCollection.insertOne(newDocument);
+    gifsAddedCounter.inc();
     res.redirect("/");
   });
 
